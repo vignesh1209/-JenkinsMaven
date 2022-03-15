74b8d450d9ccea0b87dc0757e75704e478cfab33
vignesh1209 <vignesh26lti@gmail.com>
2022-03-15 23:42:02 +0530

    first commit

--- JenkinsMaven/src/main/java/com/lti/Welcome.java
+++ JenkinsMaven/src/main/java/com/lti/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("hey welcome");
+      System.out.println("welcome");
      
 	}
 
